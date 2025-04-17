f21799908265c2dcac3112847622d37467b68ecf
AndreaDeBlasio95 <andrea@whoknocks.io>
2025-04-18 00:50:36 +0200

    -

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,11 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/react-unity-showcase-demo/", // This stays the same as it's the repo name
+  base: "/react-unity-showcase-demo/",
   build: {
     outDir: "dist",
-    assetsDir: "assets",
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-      },
-    },
+    assetsInlineLimit: 0, // Don't inline any assets as base64
   },
 });
